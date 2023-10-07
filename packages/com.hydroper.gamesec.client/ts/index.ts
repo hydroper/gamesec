3a3ab41c81aacf45c43b7b4ce92c47b747a8b847
hydroper <matheusdiasdesouzads@gmail.com>
2023-10-07 18:22:58 -0300

    _

--- packages/com.hydroper.gamesec.client/ts/index.ts
+++ packages/com.hydroper.gamesec.client/ts/index.ts
@@ -1,5 +1,6 @@
 export * from "com.hydroper.gamesec.core";
 export * from "com.hydroper.gamesec.display2D";
+export * from "com.hydroper.gamesec.ui";
 
 import Input from "./Input";
 export { Input };
