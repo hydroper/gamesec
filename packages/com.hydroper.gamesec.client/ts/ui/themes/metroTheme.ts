01b4fe89f53bdc09dad82e7272ebc9b14874f805
hydroper <matheusdiasdesouzads@gmail.com>
2023-10-10 15:51:23 -0300

    _

--- packages/com.hydroper.gamesec.client/ts/ui/themes/metroTheme.ts
+++ packages/com.hydroper.gamesec.client/ts/ui/themes/metroTheme.ts
@@ -3,13 +3,17 @@ import { Theme } from "./Theme";
 /**
  * Theme based on Microsoft Metro design.
  */
-export const metroTheme = new Theme("metro", {
-    defaultFont: "'Segoe Ui', 'Open Sans', 'Noto Sans', sans",
-    fonts: [],
-    controls: {
-        application: {
-            background: "#fff",
-            fontColor: "#0a0a0a",
+export const metroTheme = createMetroTheme();
+
+function createMetroTheme() {
+    return new Theme("metro", {
+        defaultFont: "'Segoe Ui', 'Open Sans', 'Noto Sans', sans",
+        fonts: [],
+        controls: {
+            application: {
+                background: "#222",
+                fontColor: "#fff",
+            },
         },
-    },
-});
+    });
+}
